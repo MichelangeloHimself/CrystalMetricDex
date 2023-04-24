adbd01dec9b3c36a98de26d6bd81399fd6c4a136
MichelangeloHimself <102033560+MichelangeloHimself@users.noreply.github.com>
2023-04-25 05:59:23 +1200

    fix 151 Original Entries

---
+++ data/pokemon/dex_entries/starmie.asm
@@ -0,0 +1,10 @@
+	db "MYSTERIOUS@" ; species name
+	dw 110, 800 ; height, weight
+	; METRIC
+	db   "It is said that it"
+	next "uses the seven-"
+	next "colored core of"
+
+	page "its body to send"
+	next "electric waves"
+	next "into outer space.@"
